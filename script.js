98a986294f0853ac5f5934e5d6dd4e1e6728184e
kjduncan <katelynduncan@Katelyns-MacBook-Pro.local>
2018-10-06 21:50:24 -0400

    save data

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
   var defaultOrder = [
     {name: "original",
     options: {
-      glaze: "cream cheese",
+      glaze: "creamCheese",
       quantity: 6,
     },
       id: 1,
@@ -12,7 +12,7 @@
     },
     {name: "blueberry",
       options: {
-        glaze: "cream cheese",
+        glaze: "creamCheese",
         quantity: 6,
       },
       id: 2,
@@ -20,7 +20,7 @@
     },
     {name: "caramel_pecan",
       options: {
-        glaze: "cream cheese",
+        glaze: "creamCheese",
         quantity: 6,
       },
       id: 3,
@@ -28,7 +28,7 @@
     },
     {name: "walnut",
       options: {
-        glaze: "cream cheese",
+        glaze: "creamCheese",
         quantity: 6,
       },
       id: 4,
@@ -36,7 +36,7 @@
     },
     {name: "citrus",
       options: {
-        glaze: "cream cheese",
+        glaze: "creamCheese",
         quantity: 6,
       },
       id: 5,
@@ -44,22 +44,63 @@
     },
     {name: "pumpkin_spice",
       options: {
-        glaze: "cream cheese",
+        glaze: "creamCheese",
         quantity: 6,
       },
       id: 6,
 
     }
   ]
+  function optionsVal (options) {
+    var selectedOption;
+      for (var i = 0; i < options.length; i++) {
+        if (options[i].checked){
+          selectedOption = options[i].value;
+          break;
+        }
+      }
+      return selectedOption;
+
+  };
+
+  function buildCartItem (id, glaze, quantity) {
+    var cartObject = {
+      options: {
+        glaze: glaze,
+        quantity: quantity,
+      },
+      id: id,
+    };
+    cartObject.name = defaultOrder[id - 1].name;
+    return cartObject;
+
+
+  };
 
   var shoppingCart = [];
 
   function addToCart(e) {
     if (e && e.target) {
       e.preventDefault();
-      console.warn(e.target.id);
-      console.warn(_.find(defaultOrder, function(o) { return o.id === parseInt(e.target.id); }));
-      shoppingCart.push(_.find(defaultOrder, function(o) { return o.id === parseInt(e.target.id); }));
+
+      var quantityOptions;
+      var glazeOptions;
+
+      if (document.getElementsByName('quantity').length || document.getElementsByName('glaze').length) {
+        quantityOptions = optionsVal(document.getElementsByName('quantity'));
+        glazeOptions = optionsVal(document.getElementsByName('glaze'));
+      } else {
+        console.warn(defaultOrder[(e.target.id - 1)].options.quantity);
+        quantityOptions = defaultOrder[(e.target.id - 1)].options.quantity;
+        glazeOptions = defaultOrder[e.target.id - 1].options.glaze;
+      }
+
+      var shoppingCartItem = buildCartItem(e.target.id, glazeOptions, quantityOptions);
+
+      shoppingCart.push(shoppingCartItem);
+      console.dir(e.target);
+      console.warn(shoppingCart);
+      // shoppingCart.push(_.find(defaultOrder, function(o) { return o.id === parseInt(e.target.id); }));
     }
     console.warn(shoppingCart);
   };
